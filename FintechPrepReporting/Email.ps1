e00d6081685a389acc650476089ee8ee36e219aa
fintechprep <fintechprep@gmail.com>
2022-12-15 15:43:51 -0700

    India Foreign Reserves
    India Foreign Reserves

---
+++ FintechPrepReporting/Email.ps1
@@ -0,0 +1,9 @@
+﻿$From = "anih07@gmail.com"
+$To = "anih07@gmail.com"
+$Subject = "Email Subject"
+$Body = "Insert body text here"
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+Send-MailMessage -From $From -to $To -Subject $Subject `
+-Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl `
+-Credential (Get-Credential) 
